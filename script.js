cf5474e16a433718ba0a6a1a9fefb2d039064db8
Anthony Chaiditya <achaiditya@gmail.com>
2024-05-20 17:34:58 -0400

    updated js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const answerOne = document.getElementById("answer-one");
-console.log(answerOne.style)
+const answerTwo = document.getElementById("answer-two");
+const answerThree = document.getElementById("answer-three");
+const answerFour = document.getElementById("answer-four");
+
 
 
 const plusButtonImage = ''
@@ -13,17 +16,46 @@ const buttonThree = document.getElementById("button-three");
 const buttonFour = document.getElementById("button-four");
 console.log(buttonOne.innerHTML)
 
-buttonOne.addEventListener("click", (event) => {
+buttonOne.addEventListener("click", () => {
   // if the display is there -> make it disappear, and make sure the button shows to '+'
-  if (answerOne.style.display === "flex") {
-    answerOne.style.display === "none";
+  if (answerOne.style.display !== "none") {
+    answerOne.style.display = "none";
     buttonOne.innerHTML = '<img src="/assets/images/icon-plus.svg" alt="button-img">';
-  } 
-
-  // if the display is not there -> make it appear, and make sure the button shows to 'e
-  if (buttonOne.style.display === "none") {
-    buttonOne.style.display === "flex";
-    buttonOne.innterHTML = '<img src="/assets/images/icon-minus.svg" alt="button-img">';
+  } else {
+    answerOne.style.display = "flex";
+    buttonOne.innerHTML = '<img src="/assets/images/icon-minus.svg" alt="button-img">';
   }
+})
 
+buttonTwo.addEventListener("click", () => {
+  // if the display is there -> make it disappear, and make sure the button shows to '+'
+  if (answerTwo.style.display !== "none") {
+    answerTwo.style.display = "none";
+    buttonTwo.innerHTML = '<img src="/assets/images/icon-plus.svg" alt="button-img">';
+  } else {
+    answerTwo.style.display = "flex";
+    buttonTwo.innerHTML = '<img src="/assets/images/icon-minus.svg" alt="button-img">';
+  }
+})
+
+buttonThree.addEventListener("click", () => {
+  // if the display is there -> make it disappear, and make sure the button shows to '+'
+  if (answerThree.style.display !== "none") {
+    answerThree.style.display = "none";
+    buttonThree.innerHTML = '<img src="/assets/images/icon-plus.svg" alt="button-img">';
+  } else {
+    answerThree.style.display = "flex";
+    buttonThree.innerHTML = '<img src="/assets/images/icon-minus.svg" alt="button-img">';
+  }
+})
+
+buttonFour.addEventListener("click", () => {
+  // if the display is there -> make it disappear, and make sure the button shows to '+'
+  if (answerFour.style.display !== "none") {
+    answerFour.style.display = "none";
+    buttonFour.innerHTML = '<img src="/assets/images/icon-plus.svg" alt="button-img">';
+  } else {
+    answerFour.style.display = "flex";
+    buttonFour.innerHTML = '<img src="/assets/images/icon-minus.svg" alt="button-img">';
+  }
 })
